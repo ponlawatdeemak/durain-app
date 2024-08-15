e78c4dfa1a89d0814836633785abf081a1c0e36f
Anirut Nimsongprasert <anirutn@thaicom.net>
2024-08-15 20:13:22 +0700

    [style] add, custom fonts and prepare mui theme; [yarn] fix, add missing base packages;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import theme from "../styles/theme";
 import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+export default MyApp;
