2f15cef1e347c8b82881011b2e6fb5ba85e70b82
Suthika Wongsiridech <suthika.w@mapboss.co.th>
2024-08-27 13:32:26 +0700

    add change language to app bar and overview page

--- components/Layout/Menu/AppBar/AppBar.tsx
+++ components/Layout/Menu/AppBar/AppBar.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import styles from './AppBar.module.css'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import UserAvatar from '@/components/UserAvatar'
+import { Profile } from 'next-auth'
+import { useTranslation } from 'next-i18next'
+import { Button, Typography } from '@mui/material'
+import { NEXTAUTH_PROVIDER_ID } from '@/webapp.config'
 
 const AppBar = () => {
+	const { data: session } = useSession()
+	const user = session?.user
+	const { t } = useTranslation('common')
 	return (
 		<div className={styles.main}>
-			<div>AppBar</div>
+			<div>
+				<div>icon</div>
+				<Typography>ระบบวิเคราะห์พื้นที่ปลูกทุเรียน</Typography>
+			</div>
+			{user ? (
+				<div>
+					{/* <UserAvatar user={user as Profile} size='large' />{' '} */}
+					<Button variant='contained' onClick={() => signOut()}>
+						{t('LOGOUT')}
+					</Button>
+				</div>
+			) : (
+				<Button variant='contained' onClick={() => signIn(NEXTAUTH_PROVIDER_ID)}>
+					{t('LOGIN')}
+				</Button>
+			)}
 		</div>
 	)
 }
--- pages/overview/index.tsx
+++ pages/overview/index.tsx
@@ -1,7 +1,23 @@
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import nextI18NextConfig from '../../next-i18next.config.js'
+
 import PageContainer from '@/components/Layout/PageContainer/PageContainer'
-import React from 'react'
+import { UserConfig, useTranslation } from 'next-i18next'
+import { DEFAULT_LOCALE } from '@/webapp.config'
+
+export const getServerSideProps: GetServerSideProps = async (context) => ({
+	props: {
+		...(await serverSideTranslations(
+			context.locale ?? DEFAULT_LOCALE,
+			['common'],
+			nextI18NextConfig as UserConfig,
+		)),
+	},
+})
 
 const OverviewPage = () => {
+	const { t } = useTranslation('common')
 	return (
 		<PageContainer>
 			<div>OverviewPage</div>
