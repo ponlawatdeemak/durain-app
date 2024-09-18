12ef214ae1b8b227f0237016e3b3f9119da16837
ThunyautN <thunyavut.n@mapboss.co.th>
2024-09-18 14:27:37 +0700

    add: registration api functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import auth from '@/api/auth'
 import overview from './overview'
 import um from './um'
 import lookup from './lookup'
+import registration from './registration'
 const service = {
 	auth,
 	um,
 	overview,
 	lookup,
+    registration
 }
 
 export default service
