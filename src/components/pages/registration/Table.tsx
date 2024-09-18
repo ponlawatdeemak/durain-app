ea3b58dee6fa1389fc53a72992985865aec15676
ThunyautN <thunyavut.n@mapboss.co.th>
2024-09-18 18:16:39 +0700

    fix: update table style

--- src/components/pages/registration/Table.tsx
+++ src/components/pages/registration/Table.tsx
@@ -27,30 +27,38 @@ export default function RegistrationTable({
 	return (
 		<TableContainer
 			className={classNames(
-				'h-full [&&::-webkit-scrollbar-thumb]:rounded [&&::-webkit-scrollbar-thumb]:bg-[#2F7A59] [&::-webkit-scrollbar-track]:rounded [&::-webkit-scrollbar-track]:bg-[#EAF2EE] [&::-webkit-scrollbar]:w-[5px]',
-				isDesktop ? 'absolute' : '',
+				'h-full [&&::-webkit-scrollbar-thumb]:rounded [&&::-webkit-scrollbar-thumb]:bg-[#2F7A59] [&::-webkit-scrollbar-track]:rounded [&::-webkit-scrollbar-track]:bg-[#EAF2EE] [&::-webkit-scrollbar]:w-[5px] [&_.MuiTable-root]:border-separate [&_.MuiTable-root]:border-spacing-y-[4px]',
+				isDesktop ? 'absolute px-[3px]' : '',
 			)}
 		>
 			<Table stickyHeader>
-				<TableHead>
+				<TableHead className='[&_.MuiTableCell-root]:border-none [&_.MuiTableCell-root]:px-[16px] [&_.MuiTableCell-root]:py-[16px]'>
 					<TableRow className='[&>th]:bg-[#F0F2F4] [&>th]:text-[16px]'>
-						<TableCell>{t('registration:area')}</TableCell>
+						<TableCell className='rounded-l-[4px]'>{t('registration:area')}</TableCell>
 						<TableCell align='right'>{t('registration:GS02')}</TableCell>
-						<TableCell align='right'>{t('registration:nonGS02')}</TableCell>
+						<TableCell className='rounded-r-[4px]' align='right'>
+							{t('registration:nonGS02')}
+						</TableCell>
 					</TableRow>
 				</TableHead>
-				<TableBody>
+				<TableBody className='[&_.MuiTableCell-root]:px-[16px] [&_.MuiTableCell-root]:py-[12px]'>
 					{data?.adms.map((row) => (
 						<TableRow
-							className='[&>td]:font-medium'
+							className='hover:!bg-[#F2F6F0] [&>td]:font-medium'
 							hover
 							tabIndex={-1}
 							key={row.admCode}
 							onClick={() => handleRowClick(row.admCode, row.admName)}
 						>
-							<TableCell>{row.admName[language] ?? '-'}</TableCell>
-							<TableCell align='right'>{row.registeredArea[areaUnit] ?? '-'}</TableCell>
-							<TableCell align='right'>{row.nonRegisteredArea[areaUnit] ?? '-'}</TableCell>
+							<TableCell className='rounded-l-[4px] border-l-[1px] border-t-[1px]'>
+								{row.admName[language] ?? '-'}
+							</TableCell>
+							<TableCell sx={{ borderWidth: '1px 0px' }} align='right'>
+								{row.registeredArea[areaUnit] ?? '-'}
+							</TableCell>
+							<TableCell className='rounded-r-[4px] border-r-[1px] border-t-[1px]' align='right'>
+								{row.nonRegisteredArea[areaUnit] ?? '-'}
+							</TableCell>
 						</TableRow>
 					))}
 				</TableBody>
