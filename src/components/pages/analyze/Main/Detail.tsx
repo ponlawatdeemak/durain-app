05568b035ad3bca524cc6305a1902513d7d59873
Warutniy <warut.n@mapboss.co.th>
2024-10-10 00:28:32 +0700

    fix: create useOrderByFilter context

--- src/components/pages/analyze/Main/Detail.tsx
+++ src/components/pages/analyze/Main/Detail.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MapDetail from '../Detail/MapDetail'
 import FilterDetail from '../Detail/FilterDetail'
+import useOrderByFilter, { OrderBy } from '../Filter/context'
 
 const AnalyzeDetail = () => {
+	const { setFilter } = useOrderByFilter()
+
+	useEffect(() => setFilter(OrderBy.Age), [])
+
 	return (
 		<div className='flex flex-col gap-12 bg-[#F2F5F8]'>
 			<FilterDetail />
